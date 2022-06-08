68b965825b59239079bd3754606917392c92510a
Jose Bohorquez <bd567358546@gmail.com>
2022-06-08 16:22:58 -0500

    se mejora la vista de los servicios y se inplementan 3 mas en los no presenciales

--- Vistas/Paginas/servicios.php
+++ Vistas/Paginas/servicios.php
@@ -1,13 +1,14 @@
 <br><h1 class="text-center">Servicios Disponibles</h1>
 
 <div class="container"><br>
+	<div class="container"><br>
 	<div class="card  d-flex justify-content-center d-flex align-items-center d-flex align-self-center"><br>
 		<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
 		  <li class="nav-item" role="presentation">
-		    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Presenciales</button>
+		    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Serv. Presenciales</button>
 		  </li>
 		  <li class="nav-item" role="presentation">
-		    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">(NO) Presenciales</button>
+		    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Serv. (NO) Presenciales</button>
 		  </li>
 		</ul>
 		<div class="tab-content" id="pills-tabContent"><br>
@@ -17,7 +18,7 @@
   
   <div class="col">
   	<div class="card">
-      <figure class="text-center"><img src="https://static.wixstatic.com/media/316a4e_19e4620b0c174722ab71d6071b4cec60~mv2.jpg/v1/fill/w_850,h_478,al_c,q_85/316a4e_19e4620b0c174722ab71d6071b4cec60~mv2.webp" class="" height="200" width="300"></figure>
+      <figure class="text-center"><img src="https://static.wixstatic.com/media/316a4e_19e4620b0c174722ab71d6071b4cec60~mv2.jpg/v1/fill/w_850,h_478,al_c,q_85/316a4e_19e4620b0c174722ab71d6071b4cec60~mv2.webp" class="" height="150" width="250"></figure>
 	  <div class="card-body">
 	    <h5 class="card-title text-center">Mantenimiento Completo</h5>
 	    <p class="card-text">limpieza, mantenimiento preventivo y correctivo a profundidad del equipo en sus componentes de hardware, software y todos sus perifericos</p>
@@ -36,7 +37,7 @@
 
   <div class="col">
   	<div class="card">
-      <figure class="text-center"><img src="https://media.kingston.com/kingston/content/ktc-custom-personal-improve-performance.jpg" class="" height="200" width="300"></figure>
+      <figure class="text-center"><img src="https://media.kingston.com/kingston/content/ktc-custom-personal-improve-performance.jpg" class="" height="150" width="250"></figure>
 	  <div class="card-body">
 	    <h5 class="card-title text-center">Actualizacion de Hardware</h5>
 	    <p class="card-text">diagnostico del equipo donde se evalua la posibilidad de mejorar mediante el cambio de su hardware como disco duro y memoria ram</p>
@@ -55,7 +56,7 @@
 
   <div class="col">
   	<div class="card">
-      <figure class="text-center"><img src="https://salmorejogeek.files.wordpress.com/2021/01/linux-macos-windows-logos.png" class="" height="200" width="300"></figure>
+      <figure class="text-center"><img src="https://salmorejogeek.files.wordpress.com/2021/01/linux-macos-windows-logos.png" class="" height="150" width="250"></figure>
 	  <div class="card-body">
 	    <h5 class="card-title text-center">Formateo</h5>
 	    <p class="card-text">Formateo del equipo con el sistema solicitado por el cliente, teniendo en cuenta los requisitos o parametros de cada sistema operativo </p>
@@ -75,7 +76,7 @@
 
   <div class="col">
   	<div class="card">
-      <figure class="text-center"><img src="https://i.pinimg.com/originals/9b/aa/76/9baa76794a74a1b797a26eaa9491972c.png" class="" height="200" width="300"></figure>
+      <figure class="text-center"><img src="https://i.pinimg.com/originals/9b/aa/76/9baa76794a74a1b797a26eaa9491972c.png" class="" height="150" width="250"></figure>
 	  <div class="card-body">
 	    <h5 class="card-title text-center">Instalacion de Software / Juegos</h5>
 	    <p class="card-text">Instalacion de software requerido por el cliente, teniendo en cuenta parametros legales y licencias</p>
@@ -95,7 +96,7 @@
 
   <div class="col">
   	<div class="card">
-      <figure class="text-center"><img src="https://i.pinimg.com/originals/77/58/69/775869c5d049d15ae65fc5613fec6526.jpg" class="" height="200" width="300"></figure>
+      <figure class="text-center"><img src="https://i.pinimg.com/originals/77/58/69/775869c5d049d15ae65fc5613fec6526.jpg" class="" height="150" width="250"></figure>
 	  <div class="card-body">
 	    <h5 class="card-title text-center">2 x 1 Mantenimiento</h5>
 	    <p class="card-text">2x1 en limpieza, mantenimiento preventivo y correctivo a profundidad del equipo en sus componentes de hardware, software y todos sus perifericos</p>
@@ -114,7 +115,7 @@
 
   <div class="col">
   	<div class="card">
-      <figure class="text-center"><img src="https://i.pinimg.com/originals/77/58/69/775869c5d049d15ae65fc5613fec6526.jpg" class="" height="200" width="300"></figure>
+      <figure class="text-center"><img src="https://i.pinimg.com/originals/77/58/69/775869c5d049d15ae65fc5613fec6526.jpg" class="" height="150" width="250"></figure>
 	  <div class="card-body">
 	    <h5 class="card-title text-center">2 x 1 Formateo</h5>
 	    <p class="card-text">2x1 en limpieza, mantenimiento preventivo y correctivo a profundidad del equipo en sus componentes de hardware, software y todos sus perifericos</p>
@@ -134,9 +135,74 @@
 </div>
 
 		  </div>
-		  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">...</div>
+
+		  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
+		  	<div class="container row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+
+		  	  <div class="col">
+				  	<div class="card">
+				      <figure class="text-center"><img src="https://aypsoluciones.net/wp-content/uploads/2021/04/Diseno-sin-titulo-8.png" height="200" width="300"></figure>
+					  <div class="card-body">
+					    <h5 class="card-title text-center">Mantenimiento Virtual (Software)</h5>
+					    <p class="card-text">Mantenimiento Virtual, donde se diagnostica, ajusta, configura, desinfecta y mejora el equipo a nivel software</p>
+					  </div>
+					  <ul class="list-group list-group-flush">
+					    <li class="list-group-item">Limpieza Software</li>
+					    <li class="list-group-item">Limpieza Virtual</li>
+					    <li class="list-group-item">Mantenimiento Virtual</li>
+					  </ul>
+					  <div class="card-body">
+					    <a href="#" class="btn btn-sm btn-outline-info">Mas Info</a>
+					    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Solicitar Servicio</button>
+					  </div>
+					</div>
+				  </div>
+
+				    <div class="col">
+  	<div class="card">
+      <figure class="text-center"><img src="https://1.bp.blogspot.com/-w2lzvTJIsAA/XZ01eX4TZjI/AAAAAAAAR64/gth--dtNmgkJ1JLST9H9OBCCXUaEh07xACLcBGAsYHQ/s1600/25092018-GestBd_7.png" height="150" width="250"></figure>
+	  <div class="card-body">
+	    <h5 class="card-title text-center">Bases De Datos</h5>
+	    <p class="card-text">Creacion, Modelado, desarrollo de bases de datos segun requerimientos del cliente (MYSQL) </p>
+	  </div>
+	  <ul class="list-group list-group-flush">
+	    <li class="list-group-item">Modelado DB</li>
+	    <li class="list-group-item">Creacion DB</li>
+	    <li class="list-group-item">Mejoras DB</li>
+	  </ul>
+	  <div class="card-body">
+	    <a href="#" class="btn btn-sm btn-outline-info">Mas Info</a>
+	    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Solicitar Servicio</button>
+	  </div>
+	</div>
+  </div>
+
+  				    <div class="col">
+  	<div class="card">
+      <figure class="text-center"><img src="https://codigoonclick.com/wp-content/uploads/2018/05/desarrollo-web-herramientas-768x516.jpg" height="150" width="250"></figure>
+	  <div class="card-body">
+	    <h5 class="card-title text-center">Desarrollo Web</h5>
+	    <p class="card-text">Creacion, Modelado, Maquetado, desarrollo de (Paginas Web), (Sistemas De Gestion), (Software con enfoque WEB), segun requerimientos del cliente</p>
+	  </div>
+	  <ul class="list-group list-group-flush">
+	  	<li class="list-group-item">Modelado Web</li>
+	    <li class="list-group-item">Modelado Web</li>
+	    <li class="list-group-item">Creacion Web</li>
+	    <li class="list-group-item">Mejoras Web</li>
+	  </ul>
+	  <div class="card-body">
+	    <a href="#" class="btn btn-sm btn-outline-info">Mas Info</a>
+	    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Solicitar Servicio</button>
+	  </div>
+	</div>
+  </div>				  		
+
+		  	</div><br><br>
+		  </div><br><br>
+
 		</div>
 	</div>
+</div>
 </div>
 
 
